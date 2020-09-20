97182eaca512545ce37511b38f9b0fc7f8c826c3
wladimir Rocha <wladimir.rocha@equilibrio.agency>
2020-09-20 11:02:20 -0400

    first commit with Fragments on main activity

---
+++ app/src/main/java/com/example/fragment/Fragments/DataFragment.java
@@ -0,0 +1,28 @@
+package com.example.fragment.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fragment.R;
+
+public class DataFragment extends Fragment {
+
+    public DataFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_data, container, false);
+
+        return view;
+    }
+}
